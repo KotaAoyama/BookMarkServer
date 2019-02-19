5dd7be35b6c385126850492f572606185f994f11
Kota Aoyama <hongcha811@gmail.com>
2019-02-19 18:49:50 +0900

    from HTTPServer to ThreadHTTPServer

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -44,7 +44,9 @@
 import http.server
 import requests
 import os
+import threading
 from urllib.parse import unquote, parse_qs
+from socketserver import ThreadingMixIn
 
 memory = {}
 
@@ -148,8 +150,12 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write('Failed to fetch the long URL'.encode())
 
+
+class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
+    "This is an HTTPServer that supports thread-based concurrency."
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
     server_address = ('', port)
-    httpd = http.server.HTTPServer(server_address, Shortener)
+    httpd = ThreadHTTPServer(server_address, Shortener)
     httpd.serve_forever()
